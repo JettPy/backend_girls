c643cc2bc1771136d540a9f5d11cb922da071a0b
JettPy <smel_00@bk.ru>
2024-01-14 13:05:42 +0300

    Простой сервер на flask

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,12 @@
-print("hello, web!")
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello from Sergey</h1>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
