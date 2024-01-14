a752181d061c4c663de0eb1e2c15e8b0be96d8a0
ирина <marikaira3@gmail.com>
2024-01-14 13:01:49 +0300

    Первый урок. Вера.

--- lesson01.py
+++ lesson01.py
@@ -1,1 +1,11 @@
-print("Hello, web!")
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def index():
+     return "<h1>Hello from Vera</h1>;[[]"
+
+if __name__ == "__main__":
+     app.run(debug=True, host="0.0.0.0")
+
